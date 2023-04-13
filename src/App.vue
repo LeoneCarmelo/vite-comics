5de17ee44ab247c4607c52ca4ee742c2bc0b76fc
LeoneCarmelo <carmeloleone@outlook.it>
2023-04-13 17:33:05 +0200

    Add blue strip

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,28 @@
                     'fans',
                     'news',
                     'shop',
+                ],
+                listStrip: [
+                    {
+                        img: '/img/buy-comics-digital-comics.png',
+                        title: 'digital comics'
+                    },
+                    {
+                        img: '/img/buy-comics-merchandise.png',
+                        title: 'dc merchandise'
+                    },
+                    {
+                        img: '/img/buy-comics-subscriptions.png',
+                        title: 'subscriptions'
+                    },
+                    {
+                        img: '/img/buy-comics-shop-locator.png',
+                        title: 'comic shop locator'
+                    },
+                    {
+                        img: '/img/buy-dc-power-visa.svg',
+                        title: 'dc power visa'
+                    },
                 ]
             }
         }
@@ -50,15 +72,16 @@
     </main>
 
     <!-- Blue strip -->
-    <section class="strip">
+    <section class="strip py-3">
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <ul>
-                        <li>
+                    <ul class="d-flex align-items-center mb-0">
+                        <li v-for="item in listStrip" class="d-flex mx-auto align-items-center">
                             <div class="picture">
-                                <img src="" alt="">
+                                <img :src="item.img" alt="">
                             </div>
+                            <span class="text-white ms-2">{{ item.title.toUpperCase() }}</span>
                         </li>
                     </ul>
                 </div>
@@ -106,5 +129,13 @@ main {
 
 .strip{
     background-color: #0282f9;
+}
+
+.picture {
+    width:50px;
+}
+
+.picture > img {
+    max-width: 100%;
 }
 </style>
